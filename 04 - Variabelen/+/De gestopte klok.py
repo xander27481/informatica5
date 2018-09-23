99fefcd7f8ae189625f3df3cf4ca67218ee78e90
xander27481 <42997643+xander27481@users.noreply.github.com>
2018-09-23 13:09:07 +0200

    De gestopte klok

--- 04 - Variabelen/+/De gestopte klok.py
+++ 04 - Variabelen/+/De gestopte klok.py
@@ -7,8 +7,14 @@ m_V2 = (int(input('Vertrek vriendin (minuten): '))) + (h_V2 * 60)
 h_T2 = int(input('aankomst thuis (uren): '))
 m_T2 = (int(input('aankomst thuis (minuten): '))) + (h_T2 * 60)
 
+############### VAN XANDER #################
+
 verschil_thuis = m_T2 - m_T1
+if verschil_thuis < 0:
+    verschil_thuis = 1440 + verschil_thuis
 verschil_vriendin = m_V2 - m_V1
+if verschil_vriendin < 0:
+    verschil_vriendin = 1440 + verschil_vriendin
 
 achterstand = (verschil_thuis - verschil_vriendin) / 2
 
@@ -16,6 +22,8 @@ m_V2 += achterstand
 
 minuten_klok = int(m_V2 % 60)
 uren_klok = int(m_V2 // 60)
+if uren_klok > 24:
+    uren_klok -= 24
 
 print(uren_klok)
 print(minuten_klok)
