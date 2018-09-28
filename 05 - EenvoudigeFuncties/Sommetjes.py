35c0acf6868cc20e93358af3f3c3247c42946a04
xander27481 <42997643+xander27481@users.noreply.github.com>
2018-09-28 10:47:50 +0200

    Sommetjes

---
+++ 05 - EenvoudigeFuncties/Sommetjes.py
@@ -0,0 +1,13 @@
+a = int(input('getal 1: '))
+b = int(input('getal 2: '))
+
+i = 0
+print('{:>6d}'.format(a * pow(10,i)),'+','{:<6d}'.format(b * pow(10,i)),'=',str((a * pow(10,i)) + (b * pow(10,i))))
+i += 1
+print('{:>6d}'.format(a * pow(10,i)),'+','{:<6d}'.format(b * pow(10,i)),'=',str((a * pow(10,i)) + (b * pow(10,i))))
+i += 1
+print('{:>6d}'.format(a * pow(10,i)),'+','{:<6d}'.format(b * pow(10,i)),'=',str((a * pow(10,i)) + (b * pow(10,i))))
+i += 1
+print('{:>6d}'.format(a * pow(10,i)),'+','{:<6d}'.format(b * pow(10,i)),'=',str((a * pow(10,i)) + (b * pow(10,i))))
+i += 1
+print('{:>6d}'.format(a * pow(10,i)),'+','{:<6d}'.format(b * pow(10,i)),'=',str((a * pow(10,i)) + (b * pow(10,i))))
