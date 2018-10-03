02fb2617c5eff69685daa892a761ebc8358d1f7a
xander27481 <42997643+xander27481@users.noreply.github.com>
2018-10-03 11:38:26 +0200

    Crocodile paradox

--- 05 - EenvoudigeFuncties/Sommetjes.py
+++ 05 - EenvoudigeFuncties/Sommetjes.py
@@ -11,3 +11,6 @@ i += 1
 print('{:>6d}'.format(a * pow(10,i)),'+','{:<6d}'.format(b * pow(10,i)),'=',str((a * pow(10,i)) + (b * pow(10,i))))
 i += 1
 print('{:>6d}'.format(a * pow(10,i)),'+','{:<6d}'.format(b * pow(10,i)),'=',str((a * pow(10,i)) + (b * pow(10,i))))
+
+
+print('{:>6d} + {:<6d} = {:d}'.format(a * pow(10,i),b * pow(10,i),(a * pow(10,i)) + (b * pow(10,i))))
---
+++ 06 - Condities/Crocodile paradox.py
@@ -0,0 +1,13 @@
+# wie heeft stalen zenuwen
+persoon = input('Wie heeft stalen zenuwen: ')
+
+# Programa
+if persoon == 'vader':
+    boodschap = 'Krokodil geeft kind terug'
+elif persoon == 'krokodil':
+    boodschap = 'Krokodil eet kind op'
+else:
+    boodschap = 'Moe van het denken'
+
+# Uitvoer
+print(boodschap)
